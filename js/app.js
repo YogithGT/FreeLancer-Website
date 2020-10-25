70b78905da2a7234adae4de5453cb6937dfc0a1c
kumaresh <odkumaresh@gmail.com>
2020-10-25 21:22:24 +0530

    Changes added and layers, routes added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
 angular.module("jobHunterApp", [
     "jobHunterApp.controllers",
     "jobHunterApp.services",
-    "jobHunterApp.directives"
+    "jobHunterApp.directives",
+    "ngRoute"
 ]);
+
+
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+	when("/", {templateUrl: "index.html"}).
+    when("/freelancers", { templateUrl: "partials/freelancer.html", controller: "freelancerShowController" }).
+	when("/recruiters", {templateUrl: "partials/recruiters.html", controller: "freelancerShowController"}).
+	when("/register", {templateUrl: "partials/register.html", controller:"freelancerAddController"}).
+      
+	otherwise({redirectTo: '/'});
+}]);
