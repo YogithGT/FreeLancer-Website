7b874e5e6ae12c36b7d4087ee56cca9b133c698d
kumaresh <odkumaresh@gmail.com>
2020-10-26 09:38:28 +0530

    Services, controllers and directives added

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,27 @@
 angular.module("jobHunterApp.controllers", [
-    
-]);
+    "jobHunterApp.services",
+])
+    .controller('freelancerAddController', freelancerAddController)
+    .controller('freelancerShowController', freelancerShowController);
+
+
+freelancerAddController.$inject=['$location','freelancerService'];
+function freelancerAddController($location, freelancerService){
+    var itemAdder=this;
+    itemAdder.itemName="";
+    itemAdder.itemContact="";
+    itemAdder.itemfree="";
+    itemAdder.addItem = function (){
+        freelancerService.addItem(itemAdder.itemName, itemAdder.itemContact, itemAdder.itemfree);
+        $location.path("/freelancers");
+    }
+}
+            
+freelancerShowController.$inject=['freelancerService'];
+function freelancerShowController(freelancerService){
+    var showList=this;
+    showList.items=freelancerService.getItems();
+    showList.removeItem = function (itemIndex) {
+        freelancerService.removeItem(itemIndex);
+    };
+}
--- js/services.js
+++ js/services.js
@@ -1,3 +1,23 @@
 angular.module("jobHunterApp.services", [
+])
+    .service("freelancerService", freelancerService);
+
+function freelancerService() {
+    var service = this;
+    var items = [];
+    service.addItem = function (itemName, Contact, free){
+        var item = {
+            name: itemName,
+            Contact: Contact,
+            free: free
+        };
+        items.push(item);
+    };
+    service.removeItem = function (itemIndex) {
+        items.splice(itemIndex, 1);
+    }
     
-]);
+    service.getItems = function () {
+        return items;
+    }
+}
